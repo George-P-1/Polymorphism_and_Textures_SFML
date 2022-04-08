7391d41c7a68f02fb53725a0e872a5d0f7f930cc
George <100698568+George-P-1@users.noreply.github.com>
2022-04-09 01:18:39 +0200

    Polymorphism in SFML [Assignment 8]

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
+
+// Assignment 8
+void create_shapes(std::vector<std::unique_ptr<sf::Drawable>> &shapes_vec)
+{
+    std::unique_ptr<sf::CircleShape> circle = std::make_unique<sf::CircleShape>(100.0);
+    circle->setPosition(100.0, 300.0);
+    circle->setFillColor(sf::Color(100, 250, 50));
+
+    std::unique_ptr<sf::RectangleShape> rectangle = std::make_unique<sf::RectangleShape>(sf::Vector2f(120.0, 60.0));
+    rectangle->setPosition(500.0, 400.0);
+//    (*rectangle).setPosition(500.0, 400.0); // Same as using ->
+    rectangle->setFillColor(sf::Color(100, 50, 250));
+
+    auto triangle = std::make_unique<sf::ConvexShape>(); // Use auto to make things simpler
+    triangle->setPointCount(3);
+    triangle->setPoint(0, sf::Vector2f(0.0, 0.0));
+    triangle->setPoint(1, sf::Vector2f(0.0, 100.0));
+    triangle->setPoint(2, sf::Vector2f(140.0, 40.0));
+    triangle->setOutlineColor(sf::Color::Red);
+    triangle->setOutlineThickness(5);
+    triangle->setPosition(600.0, 100.0);
+
+    // emplace shapes to vector
+    shapes_vec.emplace_back(std::move(circle));
+    shapes_vec.emplace_back(std::move(rectangle));
+    shapes_vec.emplace_back(std::move(triangle));
+}
 
 int main()
 {
@@ -7,22 +36,9 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
     // create some shapes
-    sf::CircleShape circle(100.0);
-    circle.setPosition(100.0, 300.0);
-    circle.setFillColor(sf::Color(100, 250, 50));
-
-    sf::RectangleShape rectangle(sf::Vector2f(120.0, 60.0));
-    rectangle.setPosition(500.0, 400.0);
-    rectangle.setFillColor(sf::Color(100, 50, 250));
-
-    sf::ConvexShape triangle;
-    triangle.setPointCount(3);
-    triangle.setPoint(0, sf::Vector2f(0.0, 0.0));
-    triangle.setPoint(1, sf::Vector2f(0.0, 100.0));
-    triangle.setPoint(2, sf::Vector2f(140.0, 40.0));
-    triangle.setOutlineColor(sf::Color::Red);
-    triangle.setOutlineThickness(5);
-    triangle.setPosition(600.0, 100.0);
+    // Assignment 8 - Vector of sf::Drawable
+    std::vector<std::unique_ptr<sf::Drawable>> shapes;
+    create_shapes(shapes);
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -40,9 +56,11 @@ int main()
         window.clear(sf::Color::Black);
 
         // draw everything here...
-        window.draw(circle);
-        window.draw(rectangle);
-        window.draw(triangle);
+        // Assignment 8
+        for(auto &s : shapes)
+        {
+            window.draw(*s);
+        }
 
         // end the current frame
         window.display();
